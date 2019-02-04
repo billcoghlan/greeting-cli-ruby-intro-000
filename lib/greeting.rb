4ca41aa601d31cf6026451aa2a0b05004b9d2883
Bill Coghlan <bill.coghlan1@gmail.com>
2019-02-04 09:03:13 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,12 +1,3 @@
 def greeting(name)
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
-end
-
-
-
-
-def greeting(name)
-  puts "Hi! I'm HAL, what's your name? "
-  name = gets.strip.chomp
   puts "Hello #{name}. It's nice to meet you."
 end
